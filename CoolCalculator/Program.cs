2873e03baac1ba78fdfe590cfdb4d78944ef7049
PainfullyMinty <mintierthanmouthwash123@gmail.com>
2018-06-08 10:06:50 +1000

    Added random number generator

--- CoolCalculator/Program.cs
+++ CoolCalculator/Program.cs
@@ -19,12 +19,17 @@ namespace CoolCalculator
 
             Console.WriteLine("Which cool calculation do you wish to make?");
             Console.WriteLine("1. Pythagorean Theorem");
+            Console.WriteLine("2. Random Number Generator");
 
             CALCCHOICE:
             string choice = Console.ReadLine();
             if(choice == "1")
             {
                 PythagMenu();
+            }
+            else if (choice == "2")
+            {
+                RanNumMenu();
             }
             else
             {
@@ -42,7 +47,6 @@ namespace CoolCalculator
             Console.WriteLine();
             Console.WriteLine("Enter variable a: ");
             string aChoiceStr = Console.ReadLine();
-
             if (!double.TryParse(aChoiceStr, out double aChoice))
             {
                 Console.WriteLine("Invalid input! Please enter a number.");
@@ -53,7 +57,6 @@ namespace CoolCalculator
             Console.WriteLine();
             Console.WriteLine("Enter variable b: ");
             string bChoiceStr = Console.ReadLine();
-
             if (!double.TryParse(bChoiceStr, out double bChoice))
             {
                 Console.WriteLine("Invalid input! Please enter a number.");
@@ -79,6 +82,53 @@ namespace CoolCalculator
             double c = Math.Sqrt(cSqr);
 
             return c;
+        }
+
+        static void RanNumMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Random Number Generator");
+
+            MINENTER:
+            Console.WriteLine();
+            Console.WriteLine("Enter the lowest number that could be generated: ");
+            string minChoiceStr = Console.ReadLine();
+            if (!Int32.TryParse(minChoiceStr, out int minChoice))
+            {
+                Console.WriteLine("Invalid input! Please enter a number.");
+                goto MINENTER;
+            }
+
+            MAXENTER:
+            Console.WriteLine();
+            Console.WriteLine("Enter the highest number that could be generated: ");
+            string maxChoiceStr = Console.ReadLine();
+            if (!Int32.TryParse(maxChoiceStr, out int maxChoice))
+            {
+                Console.WriteLine("Invalid input! Please enter a number.");
+                goto MAXENTER;
+            }
+            if (minChoice > maxChoice)
+            {
+                Console.WriteLine("Invalid input! Please enter a number higher than the minimum.");
+                goto MAXENTER;
+            }
+
+            Console.WriteLine();
+            Console.Write("The number generated was ");
+            Console.Write(RanNum(minChoice, maxChoice));
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+            Menu();
+        }
+
+        static int RanNum(int min, int max)
+        {
+            Random rand = new Random();
+            int output = rand.Next(min, max);
+
+            return output;
         }
     }
 }
